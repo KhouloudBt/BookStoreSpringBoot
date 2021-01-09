dbd5323a77c6fb56b9217b6f695be43b590ff68c
emnak16 <emnakrbi1814@gmail.com>
2021-01-09 10:49:57 +0100

    Tests modified

--- src/test/java/tn/esprit/bookstore/MemberServiceTest.java
+++ src/test/java/tn/esprit/bookstore/MemberServiceTest.java
@@ -1,5 +1,6 @@
 package tn.esprit.bookstore;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,15 +9,24 @@ import tn.esprit.bookstore.entities.Member;
 import tn.esprit.bookstore.entities.Role;
 import tn.esprit.bookstore.views.IMemberService;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MemberServiceTest {
     @Autowired
     IMemberService memberService;
 
+    @Test
     public void addMember(){
         Member m = new Member("Khouloud","benTaoues","mail@khouloud.tn","pwd1236","12365478",Role.Administrator);
         memberService.addMember(m);
+    }
+
+    @Test
+    public void retrieveAllMembersTest(){
+        List list = memberService.retrieveAllMembers();
+        System.out.println(list);
     }
 
 
