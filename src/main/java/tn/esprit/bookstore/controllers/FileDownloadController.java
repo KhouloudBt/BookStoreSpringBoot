5595db779e37930ca563025b132856fd1cee1571
KhouloudBt <khouloud.bentaoues33@gmail.com>
2021-02-02 19:30:40 +0100

    merge with khouloud

--- src/main/java/tn/esprit/bookstore/controllers/FileDownloadController.java
+++ src/main/java/tn/esprit/bookstore/controllers/FileDownloadController.java
@@ -1,6 +1,4 @@
 package tn.esprit.bookstore.controllers;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
